e04a92ed9b96debc7e1c52f247bab5b197a95569
Abbosbek Ustaboyev Botirjonovich <102945540+AbbosbekMobiler@users.noreply.github.com>
2022-05-26 21:02:12 +0500

    Initial commit

---
+++ app/src/main/java/com/example/stylingtextcompose/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.stylingtextcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.sp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Box(modifier = Modifier
+                .fillMaxSize()
+                .background(Color(0xFF101010))){
+                Text(
+                    text = "Jetpack Compose",
+                    color = Color.White,
+                    fontSize = 30.sp
+                )
+            }
+        }
+    }
+}
